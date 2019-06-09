d314cb570f17c5d01bdee2af5139ffc322ba083c
Kirill Abramov <kirill.is.ninja.frontend@gmail.com>
2019-06-09 16:24:11 +0300

    switch added

--- src/navigation/App.js
+++ src/navigation/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import { Posts, PostDetail } from 'components';
 
 const App = () => (
   <>
-    <Route exact path="/" component={Posts} />
-    <Route path="/post/:id" component={PostDetail} />
+    <Switch>
+      <Route exact path="/" component={Posts} />
+      <Route path="/post/:id" component={PostDetail} />
+    </Switch>
   </>
 );
 
