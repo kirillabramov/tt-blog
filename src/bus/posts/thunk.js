da47f763382ececf782b564bee2eb067f7c51873
Kirill Abramov <kirill.is.ninja.frontend@gmail.com>
2019-06-09 16:42:32 +0300

    add try catch

--- src/bus/posts/thunk.js
+++ src/bus/posts/thunk.js
@@ -2,22 +2,34 @@ import { api } from 'REST';
 import { postsActions } from './actions';
 
 export const getAllPosts = async dispatch => {
-  const response = await api.posts.getAllPosts();
-  const result = await response.json();
+  try {
+    const response = await api.posts.getAllPosts();
+    const result = await response.json();
 
-  dispatch(postsActions.setPosts(result));
+    dispatch(postsActions.setPosts(result));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const getPost = dispatch => async id => {
-  const response = await api.posts.getPost(id);
-  const result = await response.json();
+  try {
+    const response = await api.posts.getPost(id);
+    const result = await response.json();
 
-  dispatch(postsActions.setPost(result));
+    dispatch(postsActions.setPost(result));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const createComment = dispatch => async body => {
-  const response = await api.posts.createComment(body);
-  const result = await response.json();
+  try {
+    const response = await api.posts.createComment(body);
+    const result = await response.json();
 
-  dispatch(postsActions.createComment(result));
+    dispatch(postsActions.createComment(result));
+  } catch (err) {
+    console.error(err);
+  }
 };
