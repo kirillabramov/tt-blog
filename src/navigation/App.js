c45a5d168d031e4bbb49479f4ed9435bfd5cf9e8
Kirill Abramov <kirill.is.ninja.frontend@gmail.com>
2019-06-09 13:07:13 +0300

    cleaning app

--- src/navigation/App.js
+++ src/navigation/App.js
@@ -3,13 +3,11 @@ import { Route } from 'react-router-dom';
 
 import { Posts, PostDetail } from 'components';
 
-const App = () => {
-  return (
-    <div>
-      <Route exact path="/" component={Posts} />
-      <Route path="/post/:id" component={PostDetail} />
-    </div>
-  );
-};
+const App = () => (
+  <>
+    <Route exact path="/" component={Posts} />
+    <Route path="/post/:id" component={PostDetail} />
+  </>
+);
 
 export default App;
