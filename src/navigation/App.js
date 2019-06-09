96fb15ee8d9e1ff061f55b89c9976277475b8b10
Kirill Abramov <kirill.is.ninja.frontend@gmail.com>
2019-06-09 16:24:38 +0300

    clean fragment

--- src/navigation/App.js
+++ src/navigation/App.js
@@ -4,12 +4,10 @@ import { Route, Switch } from 'react-router-dom';
 import { Posts, PostDetail } from 'components';
 
 const App = () => (
-  <>
-    <Switch>
-      <Route exact path="/" component={Posts} />
-      <Route path="/post/:id" component={PostDetail} />
-    </Switch>
-  </>
+  <Switch>
+    <Route exact path="/" component={Posts} />
+    <Route path="/post/:id" component={PostDetail} />
+  </Switch>
 );
 
 export default App;
